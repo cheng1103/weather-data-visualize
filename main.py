524164f5cd12172dab8d6389b23cbda3ae3add99
cheng1103 <cheng11031103@gmail.com>
2024-03-05 16:24:03 +0800

    刪除streamlit的指定連接埠號

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import os
 if __name__ == '__main__':
 
     # 主程式進入點
-    command = f'streamlit run frontend/main.py  --server.port 8501 & uvicorn backend.main:app'
+    command = f'streamlit run frontend/main.py & uvicorn backend.main:app'
     os.system(command)
